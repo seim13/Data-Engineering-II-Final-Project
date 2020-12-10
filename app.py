bba284d6fcb00561f2d082ae9054f1f2bdc111b8
Juliette <45450228+seim13@users.noreply.github.com>
2020-12-10 15:33:07 +0100

    Update file

--- app.py
+++ app.py
@@ -1,59 +1,63 @@
 import numpy as np
+import pandas as pd
 from flask import Flask, request, jsonify, render_template
 from keras.models import model_from_json
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
+from gensim.models import Word2Vec
+import nltk, string
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy import spatial
+from collections import Counter
 
 app = Flask(__name__)
 
 # load json and create model
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-# load weights into new model
-loaded_model.load_weights("model.h5")
+model = Word2Vec.load('model.bin')
 print("Loaded model from disk")
+#load df
+df = pd.read_csv('clean_tweet.csv')
+
+index2word_set = set(model.wv.index2word)
 
 @app.route('/')
 def home():
     return render_template('test.html')
 
 @app.route('/predict',methods=['POST'])
+
 def predict():
+    index2word_set = set(model.wv.index2word)
 
-    String_features = [str(x) for x in request.form.values()]
-    final_features = list(String_features)
+    def avg_feature_vector(sentence, model, num_features, index2word_set):
+        words = sentence.split()
+        feature_vec = np.zeros((num_features, ), dtype='float32')
+        n_words = 0
+        for word in words:
+            if word in index2word_set:
+                n_words += 1
+                feature_vec = np.add(feature_vec, model[word])
+        if (n_words > 0):
+            feature_vec = np.divide(feature_vec, n_words)
+        return feature_vec
 
-    tokenizer = Tokenizer(split=' ')
-    tokenizer.fit_on_texts(final_features)
-    X_test = tokenizer.texts_to_sequences(final_features)
-    X_test_pad = pad_sequences(X_test)
-
-    prediction = loaded_model.predict_classes(X_test_pad, verbose=0)
-
-    for result in prediction:
-        if result == 0:
-            output = 'negatif'
-        if result == 1:
-            output = 'positif'
     
-    return render_template('test.html', prediction_text='Your text is {}'.format(output))
+    if request.method == 'POST':
+        message = request.form['message']
+        data = str(message)
+        print('computin in action')
 
-@app.route('/results',methods=['POST'])
-def results():
+        score={}
+        for i in df['tweet']:
+            sentence2=i
+            s2_afv = avg_feature_vector(sentence2, model=model, num_features=300, index2word_set=index2word_set)
+            s1_afv = avg_feature_vector(data , model=model, num_features=300, index2word_set=index2word_set)
+            sim = 1 - spatial.distance.cosine(s1_afv, s2_afv)
+            score[sentence2]=sim
 
-    data = request.get_json(force=True)
+        my_prediction= dict(Counter(score).most_common(20))
+            
+        return render_template('test.html', prediction_text='Top 20 of tweets : {}\n --  '.format(my_prediction))
 
-    tokenizer = Tokenizer(split=' ')
-    tokenizer.fit_on_texts(list(data.values()))
-    X_test = tokenizer.texts_to_sequences(list(data.values()))
-    X_test_pad = pad_sequences(X_test)
-    
-    prediction = loaded_model.predict_classes(X_test_pad)
 
-    output = prediction[0]
-    return jsonify(output)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=True)
+if __name__ == '__main__':
+	app.run(debug=True)
