56f039b8511c1ce5953cfdb30c0b2ede5fd9073c
Juliette <45450228+seim13@users.noreply.github.com>
2020-12-15 23:26:22 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -17,6 +17,6 @@ class FlaskTests(unittest.TestCase):
 		responce = requests.get('http://localhost:5000')
 		self.assertEqual(responce.status_code, 200)
 	  
-	  
-	  if __name__ == '__main__':
-		unittest.main()	
+
+if __name__ == '__main__':
+	unittest.main()	
